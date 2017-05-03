bbeca107cf034dd544ff5c9ace174e87b09f8328
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2017-05-03 13:45:40 +0200

    more config options

--- notify.py
+++ notify.py
@@ -11,8 +11,9 @@ load_dotenv(".credentials")
 
 password = os.environ.get("EXCHANGE_PASSWORD")
 username = os.environ.get("EXCHANGE_USERNAME")
-
+outgoing_email_address = os.environ.get("EXCHANGE_OUTGOING_ADDRESS")
 soundfile = os.environ.get("NOTIFICATION_SOUND")
+exchange_server = os.environ.get("EXCHANGE_SERVER")
 
 if soundfile != "":
     # load a sound file
@@ -24,13 +25,13 @@ else:
 
 print("Initiating connection to exchange...")
 # If you want to enable the fault tolerance, create credentials as a service account instead:
-credentials = ServiceAccount(username='ckreuzberger@anx.local', password=password)
+credentials = ServiceAccount(username=username, password=password)
 
 # Set up a target account and do an autodiscover lookup to find the target EWS endpoint:
-account = Account(primary_smtp_address='ckreuzberger@anexia-it.com', credentials=credentials,
+account = Account(primary_smtp_address=outgoing_email_address, credentials=credentials,
                   autodiscover=True, access_type=DELEGATE)
 
-config = Configuration(server='exchange.anexia.at', credentials=credentials, auth_type=NTLM)
+config = Configuration(server=exchange_server, credentials=credentials, auth_type=NTLM)
 
 last_number_of_emails = account.inbox.unread_count
 
